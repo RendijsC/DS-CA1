920ae99280ed343248ca028cc23bbbcdd24e8c6c
RendijsC <cielavarendijs8@gmail.com>
2024-10-30 17:06:31 +0000

    Create games table and seed it.

--- lib/gamestore-app-stack.ts
+++ lib/gamestore-app-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+
+import * as custom from "aws-cdk-lib/custom-resources";
+import { generateBatch } from "../shared/util";
+import {games} from "../seed/games";
 
 import { Construct } from 'constructs';
 
@@ -22,6 +27,31 @@ export class GameStoreAppStack extends cdk.Stack {
         allowedOrigins: ["*"],
       },
     });
+
+    const gamesTable = new dynamodb.Table(this, "GamesTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "Games",
+    });
+
+    new custom.AwsCustomResource(this, "gamesddbInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [gamesTable.tableName]: generateBatch(games),
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("gamesddbInitData"), //.of(Date.now().toString()),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [gamesTable.tableArn],
+      }),
+    });
+
+
 
     new cdk.CfnOutput(this, "GameStore Function Url", { value: gamestoreFnURL.url });
 
---
+++ seed/games.ts
@@ -0,0 +1,54 @@
+import {Game} from '../shared/types'
+
+export const games : Game[] = [
+  {
+    id: 1,
+    title: 'Mario',
+    release_year: 1973,
+    description: 'A classic platformer where Mario embarks on a quest to rescue Princess Peach from Bowser in the Mushroom Kingdom.',
+    original_language: 'en',
+    original_title: 'Mario'
+  },
+  {
+    id: 2,
+    title: 'The Legend of Zelda',
+    release_year: 1986,
+    description: 'An action-adventure game featuring Link on a quest to save Princess Zelda.',
+    original_language: 'en',
+    original_title: 'The Legend of Zelda'
+  },
+  {
+    id: 3,
+    title: 'Pac-Man',
+    release_year: 1980,
+    description: 'A classic arcade game where Pac-Man navigates mazes, eating pellets while avoiding ghosts.',
+    original_language: 'en',
+    original_title: 'Pac-Man'
+  },
+  {
+    id: 4,
+    title: 'Donkey Kong',
+    release_year: 1981,
+    description: 'A platformer where Mario must save Pauline from Donkey Kong by climbing platforms.',
+    original_language: 'en',
+    original_title: 'Donkey Kong'
+  },
+  {
+    id: 5,
+    title: 'Street Fighter II',
+    release_year: 1991,
+    description: 'A competitive fighting game where players choose characters to battle opponents.',
+    original_language: 'en',
+    original_title: 'Street Fighter II'
+  },
+  {
+    id: 6,
+    title: 'Tetris',
+    release_year: 1984,
+    description: 'A puzzle game where players rotate and arrange falling blocks to clear lines.',
+    original_language: 'en',
+    original_title: 'Tetris'
+  }
+]
+
+
